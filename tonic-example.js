67b0dfe37f536d4f86a823b6ae62b5e7b892f4cd
Comandeer <vepomoc@gmail.com>
2016-05-04 00:05:45 +0200

    [ci skip] Updated Tonic example.

--- tonic-example.js
+++ tonic-example.js
@@ -8,7 +8,7 @@ var fs = require( 'fs' ),
 
 exports.tonicEndpoint = function( request, response ) {
     const package = url.parse( request.url, true ).query.package;
-    
+
 	response.end( fs.readFileSync( require.resolve( 'bemquery-' + package ) ) );
 };
 
@@ -22,12 +22,20 @@ exports.tonicEndpoint = function( request, response ) {
 		</head>
 		<body>
 			<p>Example</p>
-			
+
             <script src="${endpoint}?package=selector-engine"></script>
             script src="${endpoint}?package=core"></script>
             script src="${endpoint}?package=async-dom"></script>
 			<script>
+				const bemQuery = bemquery.default( 'block element' );
 
+				bemQuery.html().getStates().read().then( ( values ) => {
+					const html = values[ 0 ][ 0 ];
+					const states = values[ 1 ];
+
+					console.log( html ); // Test
+					console.log( states ); // [ 'state1', 'state2' ]
+				} );
 			</script>
 		</body>
 	</html>`;
